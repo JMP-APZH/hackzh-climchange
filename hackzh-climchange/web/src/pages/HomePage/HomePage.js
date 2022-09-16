cddfc5fa520d6afb710aff371ea6e7975acc8ba4
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2022-09-17 00:18:14 +0200

    homepage background and user inputfield

--- hackzh-climchange/web/src/pages/HomePage/HomePage.js
+++ hackzh-climchange/web/src/pages/HomePage/HomePage.js
@@ -1,30 +1,57 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+import climatechangebackgrounder from 'web/src/assets/climate-change-backgrounder.jpg'
 
 const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
 
-      <h1 className='text-green-500 text-3xl'>HomePage</h1>
+      <div className='flex justify-start items-center flex-col h-screen'>
+      <div
+        // className='bg-red-300 h-screen'
+        className='relative w-full h-full'
+      >
+        <img
+          src={climatechangebackgrounder}
+          alt='homepage-bg'
+          className='w-full h-full object-cover'
+        />
+        </div>
+
+        <div className='absolute flex flex-col justify-center items-center top-0 right-0 bottom-0 left-0 bg-blackOverlay'>
+      {/* <h1 className='text-green-500 text-3xl bg-gray-300'>HomePage</h1> */}
 
       <div className='p-4 border border-solid bg-gray-300'>
-        <form>
-          <label>
-            User Input
-            <input type="text" name="name" />
+        <form className='flex flex-col'>
+          <label className='text-center'>
+            <p>User Year of Birth*</p>
+            <input
+              type="date"
+              name="name"
+              placeholder='Year of Birth'
+              className='text-center'
+
+            />
           </label>
-          <input type="submit" value="Submit" />
+
+          <input
+            type="submit"
+            value="Submit"
+            onSubmit={() => {}}
+            />
         </form>
       </div>
 
       {/* <p>
         Find me in <code>./web/src/pages/HomePage/HomePage.js</code>
       </p> */}
-      <p>
+      {/* <p>
         My default route is named <code>home</code>, link to me with `
         <Link to={routes.home()}>Home</Link>`
-      </p>
+      </p> */}
+      </div>
+      </div>
     </>
   )
 }
