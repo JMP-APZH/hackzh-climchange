5e4281b01e6aa6ed778c71b2e8aadce21f0d2633
gigibot-tech <55855355+gigibot-tech@users.noreply.github.com>
2022-09-17 11:29:29 +0200

    Added react-novel
    somehow bugged in the background
    +buttons not working

--- hackzh-climchange/web/src/pages/HomePage/HomePage.js
+++ hackzh-climchange/web/src/pages/HomePage/HomePage.js
@@ -1,12 +1,19 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 import climatechangebackgrounder from 'web/src/assets/climate-change-backgrounder.jpg'
+import  App  from '../../AppNovel.js'
 
 const HomePage = () => {
   return (
     <>
-      <MetaTags title="Home" description="Home page" />
+    <div className="container" >
+         <App />
+      </div>
 
+      <MetaTags title="Home" description="Home page" />
+      <p>
+
+      </p>
       <div className='flex justify-start items-center flex-col h-screen'>
       <div
         // className='bg-red-300 h-screen'
@@ -41,8 +48,8 @@ const HomePage = () => {
             onSubmit={() => {}}
             />
         </form>
+      
       </div>
-
       {/* <p>
         Find me in <code>./web/src/pages/HomePage/HomePage.js</code>
       </p> */}
